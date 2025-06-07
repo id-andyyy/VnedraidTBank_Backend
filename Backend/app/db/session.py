ea881fc91a0989e7c65d47403d7421438f5da451
Andy <obrezkov.andy@gmail.com>
2025-06-07 11:59:55 +0300

    Add structure

---
+++ Backend/app/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine, Engine
+from sqlalchemy.orm import sessionmaker, Session
+
+from Backend.app.core.config import settings
+
+engine: Engine = create_engine(
+    url=settings.DATABASE_URL_psycopg,
+    pool_pre_ping=True,
+)
+
+SessionLocal: sessionmaker[Session] = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+    expire_on_commit=False,
+)
---
+++ Backend/app/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import logging
+
+from sqlalchemy.exc import IntegrityError
+
+from app.api.routes import main_router
+
+app = FastAPI(
+    title="",
+    description="",
+    version="0.1.0"
+)
+
+
+@app.exception_handler(IntegrityError)
+async def integrity_error_handler(request: Request, exc: IntegrityError):
+    return JSONResponse(
+        status_code=400,
+        content={"message": f"Database integrity error: {exc.orig}"}
+    )
+
+
+app.include_router(main_router)
+
+
+@app.get(
+    "/api/health",
+    description="Health check endpoint.",
+    tags=["Health Check 👌"]
+)
+async def health_check():
+    return {"status": "ok"}
