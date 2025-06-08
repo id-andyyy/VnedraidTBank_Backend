9624eb27acc708bd0d562c811ec15f7546e19b82
Andy <obrezkov.andy@gmail.com>
2025-06-08 03:51:05 +0300

    Reorganize API router structure for improved clarity and functionality

--- app/api/routes/__init__.py
+++ app/api/routes/__init__.py
@@ -12,19 +12,19 @@ from app.api.routes.users import users_router
 main_router = APIRouter()
 
 # СЮДА ПОДКЛЮЧАТЬ РОУТЕРЫ
-main_router.include_router(pulse_router, prefix="/api/pulse", tags=["Pulse 💬"])
-main_router.include_router(llm_router, prefix="/api/llm", tags=["LLM 🤖"])
 main_router.include_router(auth_router, prefix="/api/auth", tags=["Auth 🔑"])
 main_router.include_router(
-    tradingview_router, prefix="/api/tradingview", tags=["TradingView 📈"]
-)
-main_router.include_router(
-    parsers_router, prefix="/api/parsers", tags=["Parsers 🔍"]
-)
+    users_router, prefix="/api/users", tags=["Users 🙋‍♂️"])
+main_router.include_router(news_router, prefix="/api/news", tags=["News 🗞️"])
 main_router.include_router(
     invest_router, prefix="/api/invest", tags=["Invest 💸"])
 main_router.include_router(
-    tradingview_router, prefix="/api/tradingview", tags=["Companies 🏢"])
-main_router.include_router(news_router, prefix="/api/news", tags=["News 🗞️"])
+    tradingview_router, prefix="/api/tradingview", tags=["Companies parsers 🏢"])
 main_router.include_router(
-    users_router, prefix="/api/users", tags=["Users 🙋‍♂️"])
+    parsers_router, prefix="/api/parsers", tags=["News parsers 🔍"]
+)
+main_router.include_router(pulse_router, prefix="/api/pulse", tags=["Pulse 💬"])
+main_router.include_router(llm_router, prefix="/api/llm", tags=["LLM 🤖"])
+main_router.include_router(
+    tradingview_router, prefix="/api/tradingview", tags=["TradingView 📈"]
+)
